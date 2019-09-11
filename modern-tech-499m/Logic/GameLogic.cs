4da853bbc1b8ca0314be13c59ba69a49c301369e
Сергей <greywolf.com@lmail.ru>
2019-09-11 22:22:09 +0300

    Trying to add unit tests

--- modern-tech-499m/Logic/GameLogic.cs
+++ modern-tech-499m/Logic/GameLogic.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: InternalsVisibleTo("LogicTests")]
 namespace modern_tech_499m.Logic
 {
     class GameLogic
